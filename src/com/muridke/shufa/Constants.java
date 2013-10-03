aff684896d95f4852f22ab2017515b69531d0f2c
irfansh <irfan@irfan-HP-ProBook-4540s.(none)>
2013-10-03 18:24:26 +0500

    Testing

--- src/com/muridke/shufa/Constants.java
+++ src/com/muridke/shufa/Constants.java
@@ -1,7 +1,7 @@
 package com.muridke.shufa;
 
 public class Constants {
-	//this is the contents
+	//this is the costants
 	public static final String API_KEY = "455569771217577";
 	public static final String APP_SECRET = "340c5a2c9a4076a2b4c630f759f806ac";
 	
